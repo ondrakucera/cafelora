80a58c7c66ce0e82f09d1c649673508236bd1e95
Ondra Kučera <ondrej.kucera@centrum.cz>
2025-05-03 10:04:47 +0200

    Move Header to its own component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,11 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import { Header } from "../components/Header/Header.jsx";
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
-    <header>
-      <div className="header__content container">
-        <div className="site-logo"></div>
-
-        <div className="navigation">
-          <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
-            <a href="#home">domů</a>
-            <a href="#menu">menu</a>
-            <a href="#gallery">galerie</a>
-            <a href="#contact">kontakt</a>
-          </nav>
-        </div>
-
-      </div>
-    </header>
+    <Header />
     <main>
       <section className="banner">
         <div className="container">
