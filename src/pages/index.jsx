9d18ed1f40d5a15d3edbc01175c0da7db537ff44
Ondra Kučera <ondrej.kucera@centrum.cz>
2025-05-03 10:24:02 +0200

    Move Contact to its own component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import { Header } from "../components/Header/Header.jsx";
 import { Banner } from "../components/Banner/Banner.jsx";
 import { Menu } from "../components/Menu/Menu.jsx";
 import { Gallery } from "../components/Gallery/Gallery.jsx";
+import { Contact } from "../components/Contact/Contact.jsx";
 
 document.querySelector('#root').innerHTML = render(
   <div className="page">
@@ -13,30 +14,7 @@ document.querySelector('#root').innerHTML = render(
       <Banner />
       <Menu />
       <Gallery />
-      <section>
-        <div className="container">
-          <h2>Kde nás najdete</h2>
-          <div className="contact">
-            <div className="contact__address">
-              Na Mýtě 240 <br />
-              514 03 Byšky nad Jizerou <br />
-              Tel: +420 775 334 427
-            </div>
-            <table className="contact__hours">
-              <tr><td>Pondělí</td><td>ZAVŘENO</td></tr>
-              <tr><td>Úterý</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Středa</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Čtvrtek</td><td>11:00 - 22:00</td></tr>
-              <tr><td>Sobota</td><td>9:00 - 02:00</td></tr>
-              <tr><td>Neděle</td><td>9:00 - 02:00</td></tr>
-            </table>
-            <img
-              className="contact__map"
-              src="./img/map.png"
-            />
-          </div>
-        </div>
-      </section>
+      <Contact />
     </main>
 
     <footer>
